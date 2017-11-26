601602ece9cfd1e661eaa48a628c46fbcdf8acc9
digvijay442 <32786965+digvijay442@users.noreply.github.com>
2017-11-26 22:18:42 +0530

    Update dbconnection.js

--- dbconnection.js
+++ dbconnection.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose'),
     db;
 
- db = mongoose.connect('mongodb://ds117156.mlab.com:17156/dbresults', { useMongoClient: true, user: '840340', pass: '840340'} ,(err, database)=>{
+ db = mongoose.connect('mongodb://ds117156.mlab.com:17156/dbresults', { useMongoClient: true, user: '', pass: ''} ,(err, database)=>{
     if(err)return console.log(err)
     db = database;
   })
